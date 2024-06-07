b5039fabd561694d3c9be52712f244257b058331
fariborz0015 <fariborz0015@gmail.com>
2024-06-07 13:44:45 +0330

    initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export function App(props) {
+  return (
+    <div className='metro'>
+      <div className="metors-container">
+        <div className="meteor-1"></div>
+        <div className="meteor-2"></div>
+        <div className="meteor-4"></div>
+        <div className="meteor-6"></div>
+        <div className="meteor-7"></div>
+        <div className="meteor-9"></div>
+        <div className="meteor-10"></div>
+        <div className="meteor-12"></div>
+        <div className="meteor-15"></div>
+      </div>
+    </div>
+  );
+}
+
+// Log to console
+console.log('Hello console')
